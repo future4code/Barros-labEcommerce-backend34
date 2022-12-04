e96de33ad7abdb43b9c5495c659bb49038594809
Bianca Paccola <bianca.paccola@gmail.com>
2022-12-04 16:56:41 -0300

    funcoes: add user, busca users, add produto, busca produtos

---
+++ src/data/migration.ts
@@ -0,0 +1,42 @@
+import {connection} from "./connection"
+
+const createTableUsers = () => connection.raw(
+        `
+        CREATE TABLE IF NOT EXISTS labecommerce_users (
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+        )
+        `
+    )
+
+const createTableProducts = () => connection.raw(`
+    CREATE TABLE IF NOT EXISTS labecommerce_products (
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price DECIMAL NOT NULL,
+        image_url VARCHAR(255) NOT NULL
+    )
+`)
+
+const createTablePurchases = () => connection.raw(`
+    CREATE TABLE IF NOT EXISTS labecommerce_purchases (
+        id VARCHAR(255) PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        product_id VARCHAR(255) NOT NULL,
+        quantity INT NOT NULL,
+        total_price DECIMAL NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES labecommerce_users (id),
+        FOREIGN KEY (product_id) REFERENCES labecommerce_products (id)
+    )
+`)
+
+createTableUsers()
+.then(() => console.log("Tabela de usuários criada com sucesso na base de dados!"))
+.then(()=> createTableProducts())
+.then(() => console.log("Tabela de produtos criada com sucesso na base de dados!"))
+.then(() => createTablePurchases())
+.then(() => console.log("Tabela de compras criada com  na base de dados!"))
+.catch((error: any) => console.log(error.sqlMessage || error.message))
+.finally(() => connection.destroy())
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,83 @@
 import app from "./app";
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import { connection } from "./data/connection";
 
-app.get('/teste', (req: Request, res: Response) => {
-    res.status(200).send("Teste");
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await connection.raw(`SELECT * FROM labecommerce_users`);
+  res.status(200).send(users[0]);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email, password, passwordConfirm } = req.body;
+  let errorCode = 400;
+  try {
+    if (!name || !email || !password || !passwordConfirm) {
+      errorCode = 422;
+      throw new Error("Dados incompletos.");
+    }
+    if (
+      typeof name !== "string" ||
+      typeof email !== "string" ||
+      typeof password !== "string"
+    ) {
+      errorCode = 422;
+      throw new Error("Formato de dados inválido");
+    }
+    if (password !== passwordConfirm) {
+      errorCode = 422;
+      throw new Error("Senhas precisam ser iguais.");
+    }
+
+    await connection.raw(`
+            INSERT INTO labecommerce_users (id, name, email, password)
+            VALUES (
+                "${Date.now()}",
+                "${name}",
+                "${email}",
+                "${password}"
+            );
+        `);
+    res.status(201).send("Usuário inserido na base de dados com sucesso!");
+  } catch (err: any) {
+    res.status(errorCode).send(err.message);
+  }
+});
+
+app.get("/products", async (req: Request, res: Response) => {
+    const products = await connection.raw(`SELECT * FROM labecommerce_products`);
+    res.status(200).send(products[0]);
+})
+
+app.post("/products", async (req: Request, res: Response) => {
+  const { name, price, image } = req.body;
+  let statusCode = 400;
+  try {
+    if (!name || !price || !image) {
+      statusCode = 422;
+      throw new Error("Dados incompletos.");
+    }
+    if (typeof name !== "string" || typeof image !== "string") {
+      statusCode = 422;
+      throw new Error(
+        "Nome e url da imagem devem ser passados em formato string."
+      );
+    }
+    if (typeof price !== "number" || price <= 0) {
+      statusCode = 422;
+      throw new Error("Preço inválido.");
+    }
+
+    await connection.raw(`
+    INSERT INTO labecommerce_products (id, name, price, image_url)
+    VALUES (
+        "${Date.now()}",
+        "${name}",
+        ${price},
+        "${image}"
+    );
+`);
+    res.status(201).send("Produto inserido na base de dados com sucesso!");
+  } catch (err: any) {
+    res.status(statusCode).send(err.message);
+  }
 });

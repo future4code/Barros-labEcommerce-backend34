da0c3bad2e269c2db1a34aa82c35b889fc95a943
Bianca Paccola <bianca.paccola@gmail.com>
2022-12-04 22:05:50 -0300

    port

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import express from "express"
-
 import cors from 'cors'
+import dotenv from 'dotenv';
 
 const app = express()
-
 app.use(express.json())
-
 app.use(cors())
+dotenv.config();
 
-app.listen(3003, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log("Server is running in http://localhost:3003");
 });
 
